5cf3324a87b222e67750119beb471c7ec0afc509
Troub <matveyv2004@gmail.com>
2024-12-11 16:59:19 +0300

    Initial commit

---
+++ lib/models/list.dart
@@ -0,0 +1,42 @@
+import 'package:pks7/models/class.dart';
+
+List<Services> servicesList = <Services>[
+  Services(
+      title: 'Общий анализ крови (ОАК)',
+      price: 500,
+      days: 2,
+      inCart: 0),
+  Services(
+      title: 'Биохимический анализ крови (БХАК)',
+      price: 1200,
+      days: 1,
+      inCart: 0),
+  Services(
+      title: 'Общий анализ мочи (ОАМ)',
+      price: 300,
+      days: 1,
+      inCart: 0),
+  Services(title: 'Анализ крови на гормоны',
+      price: 1500,
+      days: 1,
+      inCart: 0),
+  Services(title: 'Анализ крови на сахар (глюкоза)',
+      price: 400,
+      days: 2,
+      inCart: 0),
+  Services(
+      title: 'Анализ крови на ВИЧ',
+      price: 1000,
+      days: 5,
+      inCart: 0),
+  Services(
+      title: 'Анализ крови на гепатиты (B, C)',
+      price: 1200,
+      days: 3,
+      inCart: 0),
+  Services(
+      title: 'Анализ кала на дисбактериоз',
+      price: 800,
+      days: 3,
+      inCart: 0),
+];
